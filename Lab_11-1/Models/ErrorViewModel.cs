35126b236d91c32297b54831704e3e0501bde2fa
Lucero Toral <lucerotoral@quickenloans.com>
2020-03-24 00:24:08 -0400

    first commit

---
+++ Lab_11-1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_11_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
